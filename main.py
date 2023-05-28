205d083aed2209b4126770f2165f5278c2a56294
Bern <103102485+NBernkastel@users.noreply.github.com>
2023-05-28 23:49:40 +0600

    2

--- main.py
+++ main.py
@@ -1,77 +1,65 @@
 import datetime
-import logging
 import requests
 from aiogram import Bot, Dispatcher, executor, types
 from bs4 import BeautifulSoup
-import re
-
-API_TOKEN = '5291964444:AAGXq6himZqCQdqYu0b-dKAAKogMlRD_tY8'
-today = datetime.datetime.now()
-day_of_week = today.weekday()
-week_number = today.isocalendar()[1]
-# Configure logging
-logging.basicConfig(level=logging.INFO)
+from utuls import URL, API_TOKEN, days_dict, schedule_print
 
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-days_dict = {
-    0: 'Пнд',
-    1: 'Втр',
-    2: 'Срд',
-    3: 'Чтв',
-    4: 'Птн',
-    5: 'Сбт',
-    6: 'Вск'
-}
-TODAY = days_dict[day_of_week]
-WEEK = True if week_number % 2 else False
-reg = r"(?<=а\.)[^.]+"
 
 
 @dp.message_handler(commands=['get'])
 async def send_welcome(message: types.Message):
-    url = 'https://portal.omgups.ru/extranet/raspisanie/semester2_2022-2023/raspisanie_iatit/40.htm'
-    response = requests.get(url)
+    time = datetime.datetime.now()
+    day_of_week = time.weekday()
+    week_number = time.isocalendar()[1]
+    today = days_dict[day_of_week]
+    week = True if week_number % 2 else False
+    response = requests.get(URL)
     soup = BeautifulSoup(response.text, 'html.parser')
     tabl = soup.find_all('tr')
     day_info = []
     del tabl[0]
     del tabl[0]
-    if WEEK:
+    if week:
         del tabl[6:]
     for days in tabl:
         day_content = days.find_all('p')
-        if day_content[0].text == TODAY:
+        if day_content[0].text == today:
             for p in day_content:
                 day_info.append(p.text)
     if day_of_week < 6:
-        out = f"""День недели- {TODAY}
-Первая пара
-Пара: {day_info[1][:len(day_info[1])-11]}
-Время: 08:00-09:30
-Аудитория: {str(re.findall(reg, day_info[1])[0] if len(re.findall(reg, day_info[1])) > 0 else '-')}
+        out = schedule_print(today, day_info)
+    else:
+        out = 'Чел спи'
+    await message.answer(out)
 
-Вторая пара 
-Пара: {day_info[2][:len(day_info[2])-11]}
-Время: 09:45-11:15
-Аудитория: {str(re.findall(reg, day_info[2])[0] if len(re.findall(reg, day_info[2])) > 0 else '-')}
 
-Третья пара 
-Пара: {day_info[3][:len(day_info[3])-11]}
-Время: 11:30-13:00
-Аудитория: {str(re.findall(reg, day_info[3])[0] if len(re.findall(reg, day_info[3])) > 0 else '-')}
-
-Четвертая пара
-Пара: {day_info[4][:len(day_info[4])-11]}
-Время: 13:55-15:25
-Аудитория: {str(re.findall(reg, day_info[4])[0] if len(re.findall(reg, day_info[4])) > 0 else '-')}
-
-Пятая пара
-Пара: {day_info[5][:len(day_info[5])-11]}
-Время: 15:40-17:10
-Аудитория: {str(re.findall(reg, day_info[5])[0] if len(re.findall(reg, day_info[5])) > 0 else '-')}
-"""
+@dp.message_handler(commands=['nex'])
+async def send_welcome(message: types.Message):
+    time = datetime.datetime.now()
+    day_of_week = time.weekday()
+    day_of_week = day_of_week + 1 if day_of_week < 6 else 0
+    print(day_of_week)
+    week_number = time.isocalendar()[1]
+    today = days_dict[day_of_week]
+    week = True if week_number % 2 else False
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tabl = soup.find_all('tr')
+    day_info = []
+    del tabl[0]
+    del tabl[0]
+    if week:
+        del tabl[6:]
+    for days in tabl:
+        day_content = days.find_all('p')
+        if day_content[0].text == today:
+            for p in day_content:
+                day_info.append(p.text)
+    if day_of_week < 6:
+        out = schedule_print(today, day_info)
     else:
         out = 'Чел спи'
     await message.answer(out)
@@ -79,5 +67,3 @@ async def send_welcome(message: types.Message):
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-    gg = []
-    gg.get(0)
--- utuls.py
+++ utuls.py
@@ -1,10 +1,45 @@
+import re
 from enum import Enum
+import logging
 
+URL = 'https://portal.omgups.ru/extranet/raspisanie/semester2_2022-2023/raspisanie_iatit/40.htm'
+API_TOKEN = '5998384248:AAGa4_UX5grl7dEHz-BjzzNwEAqaWboGSqc'
+days_dict = {
+    0: 'Пнд',
+    1: 'Втр',
+    2: 'Срд',
+    3: 'Чтв',
+    4: 'Птн',
+    5: 'Сбт',
+    6: 'Вск'
+}
+reg = r"а\..{5}"
+logging.basicConfig(level=logging.INFO)
 
-class Days(Enum):
-    Monday = 'Пнд'
-    Tuesday = 'D'
-    Wednesday = 2
-    Thursday = 3
-    Friday = 4
-    Saturday = 5
+def schedule_print(today: str, day_info: list[str]):
+    return f"""День недели- {today}
+Первая пара
+Пара: {day_info[1][:len(day_info[1]) - 11]}
+Время: 08:00-09:30
+Аудитория: {str(re.findall(reg, day_info[1])[0] if len(re.findall(reg, day_info[1])) > 0 else '-')}
+
+Вторая пара 
+Пара: {day_info[2][:len(day_info[2]) - 11]}
+Время: 09:45-11:15
+Аудитория: {str(re.findall(reg, day_info[2])[0] if len(re.findall(reg, day_info[2])) > 0 else '-')}
+
+Третья пара 
+Пара: {day_info[3][:len(day_info[3]) - 11]}
+Время: 11:30-13:00
+Аудитория: {str(re.findall(reg, day_info[3])[0] if len(re.findall(reg, day_info[3])) > 0 else '-')}
+
+Четвертая пара
+Пара: {day_info[4][:len(day_info[4]) - 11]}
+Время: 13:55-15:25
+Аудитория: {str(re.findall(reg, day_info[4])[0] if len(re.findall(reg, day_info[4])) > 0 else '-')}
+
+Пятая пара
+Пара: {day_info[5][:len(day_info[5]) - 11]}
+Время: 15:40-17:10
+Аудитория: {str(re.findall(reg, day_info[5])[0] if len(re.findall(reg, day_info[5])) > 0 else '-')}
+"""
